3f7188e1fb8266a1e5d7fe42af992c49ae2df2cf
jmlee90 <jmlee90@sk.com>
2018-08-01 14:47:03 +0900

    init

---
+++ src/main/java/com/skcc/oms/example/ExampleApplication.java
@@ -0,0 +1,38 @@
+package com.skcc.oms.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+
+import com.sk.cnaps.application.sp.web.EnumTypeRestController;
+import com.skcc.oms.example.domain.sample.service.logic.SampleLogic;
+
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+
+@Import(SpringDataRestConfiguration.class)
+@SpringBootApplication
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+@EnableFeignClients
+public class ExampleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner test(SampleLogic sampleLogic) {
+		return (args) -> {
+			sampleLogic.doSomethingManyAggregates();
+			
+			//sampleLogic.doSomethingUsingServicedesk();
+		};
+	}
+
+	@Bean
+	public EnumTypeRestController createEnumtypeRestController() {
+		return new EnumTypeRestController("com.sk");
+	}
+}
